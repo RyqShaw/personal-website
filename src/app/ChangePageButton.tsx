c90639f94abda34fc20f5fc1ff0e59963c9b2a9c
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2024-04-19 16:22:01 -0400

    Side Bar Concept

--- src/app/ChangePageButton.tsx
+++ src/app/ChangePageButton.tsx
@@ -14,7 +14,7 @@ export function ChangePageButton({
   };
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
       onClick={navigateToOtherPage}
     >
       {str}
