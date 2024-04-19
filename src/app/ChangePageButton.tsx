f5c77436ac363af8e85819ccfbf7b26987117d4f
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2024-04-19 17:59:33 -0400

    Dark Mode!

--- src/app/ChangePageButton.tsx
+++ src/app/ChangePageButton.tsx
@@ -9,7 +9,9 @@ const ChangePageButton = ({ str, location }: { str: string; location: string }) 
   };
   return (
     <button
-      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+      className="bg-blue-500 hover:bg-blue-700
+       dark:bg-red-500 dark:hover:bg-red-700
+       text-white font-bold py-2 px-4 rounded"
       onClick={navigateToOtherPage}
     >
       {str}
