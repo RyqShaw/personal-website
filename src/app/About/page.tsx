000ae2bbaf62f3beb8e5f20e21f14e28d73a292d
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2024-04-19 21:10:58 -0400

    testing something?

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,19 +1,9 @@
-import Image from 'next/image';
-
 export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl text-center font-bold underline py-5">
         About Me
       </h1>
-
-      {/* <Image
-        className="w-24 h-24 md:w-48 md:h-auto rounded-3xl mx-auto"
-        src="https://tsh.io/wp-content/uploads/2019/12/react-meme1_.png"
-        alt="Lmao"
-        width={500}
-        height={500}
-      /> */}
     </main>
   );
 }
