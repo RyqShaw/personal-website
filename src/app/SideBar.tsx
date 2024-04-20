a0432725b6c049c8d7967e98f4d3a945a2d0c767
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2024-04-19 22:11:28 -0400

    Actually Last commit: High Lights Active page

--- src/app/SideBar.tsx
+++ src/app/SideBar.tsx
@@ -3,33 +3,21 @@ import './globals.css';
 import { ReactNode } from 'react';
 import { FaAddressBook, FaBook, FaHome } from 'react-icons/fa';
 import { FaGears, FaGithub, FaLinkedin } from 'react-icons/fa6';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname  } from 'next/navigation';
 
 const SideBar = () => {
   const router = useRouter();
 
-  const navigateHome = () => {
-    router.push('/');
-  };
+  const navigateHome = () =>  router.push('/');
+  const navigateAbout = () => router.push('/aboutme');
+  const navigateProjects = () => router.push('/projects');
+  const navigateContact = () => router.push('/contact');
+  const navigateGithub = () => router.push('https://github.com/RyqShaw');
+  const navigateLinkedin = () => router.push('https://www.linkedin.com/in/sharyqsiddiqi/');
 
-  const navigateAbout = () => {
-    router.push('/aboutme');
-  };
-
-  const navigateProjects = () => {
-    router.push('/projects');
-  };
-
-  const navigateContact = () => {
-    router.push('/contact');
-  };
-
-  const navigateGithub = () => {
-    router.push('https://github.com/RyqShaw');
-  };
-
-  const navigateLinkedin = () => {
-    router.push('https://www.linkedin.com/in/sharyqsiddiqi/');
+  const pathname = usePathname();
+  const isActive = (path: string) => {
+    return pathname === path;
   };
 
   return (
@@ -39,24 +27,24 @@ const SideBar = () => {
                      flex flex-col bg-zinc-300
                      dark:bg-zinc-900 text-white shadow-lg"
       >
-          <SideBarIcon icon={<FaHome size={'32'} />} text="Home" navigate={navigateHome} />
-          <SideBarIcon icon={<FaBook size={'28'} className="pt-0.5" />} text="About Me" navigate={navigateAbout} />
-          <SideBarIcon icon={<FaGears size={'32'} />} text="Projects" navigate={navigateProjects} />
-          <SideBarIcon icon={<FaAddressBook size={'28'} className="pl-0.5 pt-0.5" />} text="Contact Me"  navigate={navigateContact}/>
+          <SideBarIcon icon={<FaHome size={'32'} />} text="Home" navigate={navigateHome} active={isActive('/')}/>
+          <SideBarIcon icon={<FaBook size={'28'} className="pt-0.5" />} text="About Me" navigate={navigateAbout} active={isActive('/aboutme')}/>
+          <SideBarIcon icon={<FaGears size={'32'} />} text="Projects" navigate={navigateProjects} active={isActive('/projects')}/>
+          <SideBarIcon icon={<FaAddressBook size={'28'} className="pl-0.5 pt-0.5" />} text="Contact Me" navigate={navigateContact} active={isActive('/contact')}/>
       </div>
       <div
         className="fixed bottom-0 left-0 w-16 m-0 justify-end
                      flex flex-col"
       >
-        <SideBarIcon icon={<FaGithub size={'28'} className="pt-0.5" />} text="GitHub" navigate={navigateGithub} />
-        <SideBarIcon icon={<FaLinkedin size={'28'} className="pt-0.5" />} text="Linkedin" navigate={navigateLinkedin} />
+        <SideBarIcon icon={<FaGithub size={'28'} className="pt-0.5" />} text="GitHub" navigate={navigateGithub} active={false}/>
+        <SideBarIcon icon={<FaLinkedin size={'28'} className="pt-0.5" />} text="Linkedin" navigate={navigateLinkedin} active={false}/>
       </div>
     </div>
   );
 };
 
-const SideBarIcon = ({ icon, text = 'tooltip', navigate }: { icon: ReactNode; text: string, navigate: any }) => (
-  <button onClick={navigate} className="sidebar-icon group">
+const SideBarIcon = ({ icon, text = 'tooltip', navigate, active = false }: { icon: ReactNode; text: string, navigate: any , active : boolean}) => (
+  <button onClick={navigate} className={`sidebar-icon group ${active ? 'active-icon' : ''}`}>
     {icon}
     <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
   </button>
