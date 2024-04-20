c64bafb1a99231bc97f8afd4dcfb03fa9576c370
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2024-04-19 21:05:17 -0400

    Uncommented Image

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -7,13 +7,13 @@ export default function About() {
         About Me
       </h1>
 
-      {/* <Image
+      <Image
         className="w-24 h-24 md:w-48 md:h-auto rounded-3xl mx-auto"
         src="https://tsh.io/wp-content/uploads/2019/12/react-meme1_.png"
         alt="Lmao"
         width={500}
         height={500}
-      /> */}
+      />
     </main>
   );
 }
