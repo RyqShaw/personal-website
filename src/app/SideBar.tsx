e00a993c51c3fd29fdbd2a873a2f5269bc6c233f
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2024-04-23 15:40:20 -0400

    Keep this side bar for now. work on the website and its content

--- src/app/SideBar.tsx
+++ src/app/SideBar.tsx
@@ -3,14 +3,15 @@ import './globals.css';
 import { ReactNode } from 'react';
 import { FaAddressBook, FaBook, FaHome } from 'react-icons/fa';
 import { FaGears, FaGithub, FaLinkedin } from 'react-icons/fa6';
-import { useRouter, usePathname  } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
 const SideBar = () => {
-
-  {/* Navigation tools */}
+  {
+    /* Navigation tools */
+  }
   const router = useRouter();
 
-  const navigateHome = () =>  router.push('/');
+  const navigateHome = () => router.push('/');
   const navigateAbout = () => router.push('/aboutme');
   const navigateProjects = () => router.push('/projects');
   const navigateContact = () => router.push('/contact');
@@ -23,28 +24,59 @@ const SideBar = () => {
   };
 
   return (
-    <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col justify-between bg-zinc-300 dark:bg-zinc-900 text-white shadow-lg'>
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col justify-between bg-zinc-300 dark:bg-zinc-900 text-white shadow-lg">
       {/* Page Links Links */}
-      <div
-        className="flex flex-col justify-center mt-32 h-full"
-      >
-          <SideBarIcon icon={<FaHome size={'32'} />} text="Home" navigate={navigateHome} active={isActive('/')}/>
-          <SideBarIcon icon={<FaBook size={'28'} className="pt-0.5" />} text="About Me" navigate={navigateAbout} active={isActive('/aboutme')}/>
-          <SideBarIcon icon={<FaGears size={'32'} />} text="Projects" navigate={navigateProjects} active={isActive('/projects')}/>
-          <SideBarIcon icon={<FaAddressBook size={'28'} className="pl-0.5 pt-0.5" />} text="Contact Me" navigate={navigateContact} active={isActive('/contact')}/>
-      </div>
-      {/* Social Media Links */}
-      <div
-        className="flex flex-col bottom-0"
-      >
-        <SideBarIcon icon={<FaGithub size={'28'} className="pt-0.5" />} text="GitHub" navigate={navigateGithub} active={false}/>
-        <SideBarIcon icon={<FaLinkedin size={'28'} className="pt-0.5" />} text="Linkedin" navigate={navigateLinkedin} active={false}/>
+      <div className="flex flex-col justify-center h-full">
+        <SideBarIcon icon={<FaHome size={'32'} />} text="Home" navigate={navigateHome} active={isActive('/')} />
+        <SideBarIcon
+          icon={<FaBook size={'28'} className="pt-0.5" />}
+          text="About Me"
+          navigate={navigateAbout}
+          active={isActive('/aboutme')}
+        />
+        <SideBarIcon
+          icon={<FaGears size={'32'} />}
+          text="Projects"
+          navigate={navigateProjects}
+          active={isActive('/projects')}
+        />
+        <SideBarIcon
+          icon={<FaAddressBook size={'28'} className="pl-0.5 pt-0.5" />}
+          text="Contact Me"
+          navigate={navigateContact}
+          active={isActive('/contact')}
+        />
+        {/* Social Media Links */}
+        <div className="flex flex-col bottom-0">
+          <SideBarIcon
+            icon={<FaGithub size={'28'} className="pt-0.5" />}
+            text="GitHub"
+            navigate={navigateGithub}
+            active={false}
+          />
+          <SideBarIcon
+            icon={<FaLinkedin size={'28'} className="pt-0.5" />}
+            text="Linkedin"
+            navigate={navigateLinkedin}
+            active={false}
+          />
+        </div>
       </div>
     </div>
   );
 };
 
-const SideBarIcon = ({ icon, text = 'tooltip', navigate, active = false }: { icon: ReactNode; text: string, navigate: any , active : boolean}) => (
+const SideBarIcon = ({
+  icon,
+  text = 'tooltip',
+  navigate,
+  active = false,
+}: {
+  icon: ReactNode;
+  text: string;
+  navigate: any;
+  active: boolean;
+}) => (
   <button onClick={navigate} className={`sidebar-icon group ${active ? 'active-icon' : ''}`}>
     {icon}
     <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
