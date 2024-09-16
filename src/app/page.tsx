bd4c104946a7ee87c014be9352343cf481d65fa2
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2024-09-16 17:19:51 -0400

    Started Grid Work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,55 +10,54 @@ export default function Home() {
     // TODO: Make a component for these small boxes to reuse\
     // TODO: Make a bigger box thats horizontal
     <main>
-      <div className="flex min-h-screen flex-col pl-24">
-        <div
-          className="bg-zinc-300 bg-opacity-95 dark:bg-zinc-900 dark:bg-opacity-95 text-zinc-800
+      {/*BOX 2*/}
+      <div className="grid grid-flow-col grid-cols-2 min-h-screen">
+        <div className="flex-col pl-24">
+
+          {/*BOX 1*/}
+          <div
+            className="bg-zinc-300 bg-opacity-95 dark:bg-zinc-900 dark:bg-opacity-95 text-zinc-800
            dark:text-white shadow-lg
             m-4 mt-8 p-8 w-fit rounded-3xl mr-12
             max-w-screen-md"
-        >
-          {/* info box 1 */}
-          <h1 className="text-3xl font-bold underline pb-5">{me.name}</h1>
-          <div className="flex flex-col md:flex-row md:justify-center md:items-center">
-            <Image
-              className="h-56 w-56 rounded-3xl object-cover"
-              src={me.imageUrl}
-              alt={'Photo of ' + me.name}
-              width={me.imageSize}
-              height={me.imageSize}
-            />
-            <p className="p-8 md:pl-8 pl-0 text-pretty text-left">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
-            </p>
+          >
+            {/* info box 1 */}
+            <h1 className="text-3xl font-bold pb-5">{me.name}</h1>
+            <div className="flex flex-col md:flex-row md:justify-center md:items-center">
+              <Image
+                className="h-56 w-56 rounded-3xl object-cover"
+                src={me.imageUrl}
+                alt={'Photo of ' + me.name}
+                width={me.imageSize}
+                height={me.imageSize}
+              />
+              <p className="p-8 md:pl-8 pl-0 text-pretty text-left text-lg">
+                Software Engineer <br></br>
+                Enthusiastic Learner <br></br>
+                Game Developer <br></br>
+                Guitarist
+              </p>
+            </div>
           </div>
-        </div>
-        <div
-          className="bg-zinc-300 bg-opacity-95 dark:bg-zinc-900 dark:bg-opacity-95
+
+          {/*BOX 2*/}
+          <div
+            className="bg-zinc-300 bg-opacity-95 dark:bg-zinc-900 dark:bg-opacity-95
            dark:text-white shadow-lg
             m-4 mt-8 p-8 w-fit rounded-3xl mr-12
             max-w-screen-md"
-        >
-          {/* info box 1 */}
-          <h1 className="text-3xl font-bold underline pb-5">Current Projects</h1>
-          <div className="flex flex-col md:flex-row md:justify-center md:items-center">
-            <Image
-              className="h-56 w-56 rounded-3xl object-cover"
-              src="https://tkahler.com/blog/wp-content/uploads/2016/11/20161121-chicago-syline-sunrise-ba01-hdr-edit.jpg"
-              alt={'Photo of ' + me.name}
-              width={me.imageSize}
-              height={me.imageSize}
-            />
-            <p className="p-8 md:pl-8 pl-0 text-pretty text-left">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
-            </p>
+          >
+            {/* info box 2 */}
+            <h1 className="text-2xl font-bold">Who Am I?</h1>
+            <div className="flex flex-col md:flex-row md:justify-center md:items-center">
+              <p className="p-8 pl-0 text-pretty text-left">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+              </p>
+            </div>
           </div>
         </div>
       </div>
